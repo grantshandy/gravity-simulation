c45ee9630afa2a6195fbd7d11934303641188c1e
grantshandy <grantshandy@gmail.com>
2022-05-19 10:29:35 -0600

    may 19, 2022 school work

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use ::glam::Vec2;
-use ::rand::{prelude::ThreadRng, thread_rng, Rng};
 use egui_macroquad::{
-    egui::{panel::Side, SidePanel},
+    egui::{Window, Frame, Style, epaint::Shadow},
     macroquad::{self, prelude::*},
 };
 
-const SPEED: f32 = 1.0;
-const GRAVITATIONAL_CONSTANT: f32 = 1.0;
+const GRAVITATIONAL_CONSTANT: f32 = 500.0;
 
 #[derive(Copy, Clone, PartialEq)]
 struct Node {
@@ -18,50 +16,40 @@ struct Node {
 
 #[macroquad::main("Gravity Simulation")]
 async fn main() {
-    let mut rng = thread_rng();
     let mut state: Vec<Node> = Vec::new();
-    let mut indices: Vec<usize> = Vec::new();
-    let mut current_radius: f32 = 10.0;
     let mut currently_selected: Option<usize> = None;
+    let mut current_radius: f32 = 10.0;
     let mut playing = false;
 
     loop {
+        let (mouse_x, mouse_y) = mouse_position();
+        let mouse_position = Vec2::new(mouse_x, mouse_y);
+        let edit_mode = is_key_down(KeyCode::LeftShift);
+        let clicking = is_mouse_button_down(MouseButton::Left);
+
         clear_background(DARKGRAY);
+        update_radius(&mut current_radius);
 
         if playing {
             calc_physx(&mut state);
+            calc_overlaps(&mut state, &mut currently_selected);
         }
 
         if is_key_down(KeyCode::R) {
             state = Vec::new();
+            currently_selected = None;
         }
 
-        let (mouse_x, mouse_y) = mouse_position();
-        if is_key_down(KeyCode::LeftShift) {
-            let mouse_wheel_y = mouse_wheel().1;
-
-            if mouse_wheel_y < 0. {
-                current_radius -= 1.0;
-                if current_radius < 5.0 {
-                    current_radius = 5.0;
+        if edit_mode {
+            if let Some(index) = overlaps_at_all((mouse_position, current_radius), &state) {
+                if clicking {
+                    currently_selected = Some(index);
                 }
-            }
+            } else if let Some(currently_selected) = currently_selected {
+                let currently_selected = state[currently_selected];
 
-            if mouse_wheel_y > 0. {
-                current_radius += 1.0;
-            }
-
-            let mut overlaps = false;
-            let mouse_position = Vec2::new(mouse_x, mouse_y);
-
-            for node in state.iter() {
-                if node.location.distance(mouse_position) < (node.mass + &current_radius) {
-                    overlaps = true;
-                    break;
-                }
-            }
-
-            if !overlaps {
+                draw_line(mouse_position.x, mouse_position.y, currently_selected.location.x, currently_selected.location.y, 4.0, RED);
+            } else {
                 draw_circle(
                     mouse_position.x,
                     mouse_position.y,
@@ -69,68 +57,40 @@ async fn main() {
                     Color::new(1.0, 1.0, 1.0, 0.5),
                 );
 
-                if is_mouse_button_down(MouseButton::Left) {
-                    indices.push(indices.len() + 1);
-
+                if clicking {
                     state.push(Node {
                         location: mouse_position,
                         velocity: Vec2::ZERO,
-                        color: random_color(&mut rng),
+                        color: WHITE,
                         mass: current_radius,
                     });
+                    currently_selected = Some(state.len() - 1);
                 }
             }
-        } else {
-            if is_mouse_button_down(MouseButton::Left) {
-                let mouse = mouse_position();
-                let mouse_position = Vec2::new(mouse.0, mouse.1);
-
-                let mut clicked_on_body = false;
-
-                for (index, node) in state.iter().enumerate() {
-                    if node.location.distance(mouse_position) < node.mass {
-                        currently_selected = Some(index);
-                        clicked_on_body = true;
-                        break;
-                    }
-                }
-
-                if !clicked_on_body {
-                    currently_selected = None;
-                }
+        } else if clicking {
+            if let Some(index) = overlaps_at_all((mouse_position, 0.0), &state) {
+                currently_selected = Some(index);
+            } else {
+                currently_selected = None;
             }
         }
 
-        for (index, node) in state.iter().enumerate() {
-            let color = match currently_selected {
-                Some(selected_index) => {
-                    if selected_index == index {
-                        RED
-                    } else {
-                        node.color
-                    }
-                }
-                None => node.color,
-            };
-
-            draw_circle(node.location.x, node.location.y, node.mass, color);
-        }
+        draw(&state, currently_selected);
 
         egui_macroquad::ui(|ctx| {
-            SidePanel::new(Side::Left, "side_panel").show(ctx, |ui| {
-                let playing_text = match playing {
-                    true => "Pause",
-                    false => "Play",
-                };
-
-                if ui.button(playing_text).clicked() {
-                    if playing {
-                        playing = false;
-                    } else {
-                        playing = true;
+            let mut shadow = Shadow::default();
+            shadow.extrusion = 0.0;
+        
+            Window::new("")
+                .title_bar(false)
+                .fixed_pos([20.0, 20.0])
+                .resizable(false)
+                .frame(Frame::window(&Style::default()).shadow(shadow))
+                .show(ctx, |ui| {
+                    if ui.button(if playing { "Pause" } else { "Play" }).clicked() {
+                        playing = opposite(playing);
                     }
-                }
-            });
+                });
         });
 
         egui_macroquad::draw();
@@ -139,38 +99,119 @@ async fn main() {
     }
 }
 
-fn calc_physx(state: &mut Vec<Node>) {
-    let state_clone = state.clone();
-
-    for node in state.iter_mut() {
-        let mut final_force = Vec2::ZERO;
-
-        for other_node in state_clone.iter() {
-            if node == other_node {
-                continue;
+fn draw(state: &Vec<Node>, currently_selected: Option<usize>) {
+    for (index, node) in state.iter().enumerate() {
+        if let Some(currently_selected) = currently_selected {
+            if index == currently_selected {
+                draw_circle(node.location.x, node.location.y, node.mass, RED);
+            } else {
+                draw_circle(node.location.x, node.location.y, node.mass, node.color);
             }
-
-            final_force += -(GRAVITATIONAL_CONSTANT
-                * ((node.mass * other_node.mass)
-                    / node.location.distance_squared(other_node.location)))
-                * ((node.location - other_node.location)
-                    / node.location.distance(other_node.location));
+        } else {
+            draw_circle(node.location.x, node.location.y, node.mass, node.color);
         }
-
-        let acceleration = final_force / node.mass;
-
-        println!("{}", acceleration);
-
-        node.velocity += acceleration * SPEED;
-        node.location += node.velocity * SPEED;
     }
 }
 
-fn random_color(rng: &mut ThreadRng) -> Color {
-    Color::from_rgba(
-        rng.gen_range(0..255),
-        rng.gen_range(0..255),
-        rng.gen_range(0..255),
-        255,
-    )
+fn update_radius(current_radius: &mut f32) {
+    let mouse_wheel_y = mouse_wheel().1;
+
+    if mouse_wheel_y < 0. {
+        *current_radius -= 1.0;
+        if *current_radius < 5.0 {
+            *current_radius = 5.0;
+        }
+    }
+
+    if mouse_wheel_y > 0. {
+        *current_radius += 1.0;
+    }
+}
+
+fn calc_overlaps(state: &mut Vec<Node>, currently_selected: &mut Option<usize>) {
+    let state_clone = state.clone();
+
+    for (index, node) in state_clone.iter().enumerate()  {
+        if let Some(overlap_index) =  overlaps_at_all((node.location, node.mass), &state_clone) {
+            if overlap_index == index {
+                continue;
+            }
+
+            let node = &mut state[index];
+            let overlap_node = &state_clone[overlap_index];
+
+            node.mass += overlap_node.mass;
+            node.velocity = (overlap_node.velocity / overlap_node.mass) + (node.velocity / node.mass);
+            node.location = if node.mass > overlap_node.mass {
+                node.location
+            } else {
+                overlap_node.location
+            };
+
+            if let Some(cs) = currently_selected {
+                if *cs == overlap_index {
+                    *currently_selected = Some(index);
+                }
+            }
+
+            state.remove(overlap_index);
+        }
+    }
+}
+
+fn calc_physx(state: &mut Vec<Node>) {
+    let state_clone = state.clone();
+
+    for index in 0..state_clone.len() {
+        let mut final_force = Vec2::ZERO;
+
+        for other_index in 0..state_clone.len() {
+            if index == other_index {
+                continue;
+            }
+
+            let node_one = &state_clone[index];
+            let node_two = &state_clone[other_index];
+
+            final_force += -(GRAVITATIONAL_CONSTANT
+                * ((node_one.mass * node_two.mass)
+                    / node_one.location.distance_squared(node_two.location)))
+                * ((node_one.location - node_two.location)
+                    / node_one.location.distance(node_two.location));
+        }
+
+        let td = 0.035;
+
+        let node = &mut state[index];
+        node.velocity += (final_force / node.mass) * td;
+        node.location += node.velocity * td;
+    }
+}
+
+fn overlaps_at_all(node: (Vec2, f32), state: &Vec<Node>) -> Option<usize> {
+    let mut o = None;
+
+    for (index, other_node) in state.iter().enumerate() {
+        if circles_overlap((node.0, node.1), (other_node.location, other_node.mass)) {
+            o = Some(index);
+        }
+    }
+
+    o
+}
+
+fn circles_overlap(node_one: (Vec2, f32), node_two: (Vec2, f32)) -> bool {
+    if node_one.0.distance(node_two.0) < (node_one.1 + node_two.1) {
+        true
+    } else {
+        false
+    }
+}
+
+fn opposite(b: bool) -> bool {
+    if b {
+        false
+    } else {
+        true
+    }
 }
